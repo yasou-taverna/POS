586c40a7066402b11ad8d1f3677bd65070aa7f07
Yasou Taverna <yasoutavern@gmail.com>
2026-08-16 22:58:27 -0400

    Add menu image map for food and drinks

---
+++ menu-image-map.js
@@ -0,0 +1,144 @@
+// Menu image map
+// Put image files in:
+// - images/menu/food
+// - images/menu/drink
+// - images/menu/alcoholic
+//
+// Example:
+// 'Coke-Zero': 'images/menu/drink/Coke-Zero.png',
+//
+// Keep empty strings until the real images arrive.
+
+window.menuImageMap = {
+    // Opening
+    // Greek-pita-with-olive-pate | Greek pita with olive pate | פיתה יוונית עם ממרח זיתים
+    'Greek-pita-with-olive-pate': 'images/menu/food/olive-pate.png',
+    // Edamame | Edamame | אדממה
+    'Edamame': 'images/menu/food/edamme.png',
+    // Crispy-French-fries | Crispy French fries | צ׳יפס קריספי
+    'Crispy-French-fries': 'images/menu/food/crispy-fries.png',
+    // Fresh-vegetable-plate | Fresh vegetable plate | פלטת ירקות טריים
+    'Fresh-vegetable-plate': '',
+    // Rice-plate | Rice plate | צלחת אורז
+    'Rice-plate': '',
+
+    // Salads
+    // Tahini-with-2-pita-breads | Tahini with 2 pita breads | טחינה בתוספת שתי פיתות
+    'Tahini-with-2-pita-breads': '',
+    // Eggplant | Eggplant | חצילים
+    'Eggplant': '',
+    // Eggplant-with-tahini | Eggplant with tahini | חצילים עם טחינה בצד
+    'Eggplant-with-tahini': '',
+    // Grilled-hot-peppers | Grilled hot peppers | פלפלים חריפים על האש
+    'Grilled-hot-peppers': '',
+    // Greek-olives | Greek olives | זיתים יווניים
+    'Greek-olives': '',
+    // Chopped-vegetable-salad | Chopped vegetable salad | סלט ירקות קצוץ
+    'Chopped-vegetable-salad': '',
+    // Yasou-salad | Yasou salad | סלט יאסו
+    'Yasou-salad': '',
+    // Vegan-Greek-Salad | Vegan Greek Salad | סלט יווני טבעוני
+    'Vegan-Greek-Salad': '',
+    // Extra-two-pita-breads | Extra two pita breads | תוספת 2 פיתות
+    'Extra-two-pita-breads': '',
+    // Extra-tahini-Zhug-hot-sauce | Extra tahini / Zhug hot sauce | תוספת טחינה / חריף סחוג
+    'Extra-tahini-Zhug-hot-sauce': '',
+
+    // Hummus
+    // Home-made-hummus | Home-made hummus | חומוס ביתי
+    'Home-made-hummus': '',
+    // Hummus-Tahini | Hummus Tahini | חומוס טחינה
+    'Hummus-Tahini': '',
+    // Hummus-with-mushrooms | Hummus with mushrooms | חומוס פטריות
+    'Hummus-with-mushrooms': '',
+    // Hummus-with-mini-schnitzel | Hummus with mini schnitzel | חומוס שניצלונים
+    'Hummus-with-mini-schnitzel': '',
+    // Hummus-with-minced-meat | Hummus with minced meat | חומוס בשר טחון
+    'Hummus-with-minced-meat': '',
+
+    // Meat & Chicken
+    // Pargit-chicken-thighs | Pargit - chicken thighs | פרגיות
+    'Pargit-chicken-thighs': '',
+    // Schnitzel | Schnitzel | שניצל
+    'Schnitzel': '',
+    // Mini-schnitzel | Mini schnitzel | שניצלונים
+    'Mini-schnitzel': '',
+    // Grilled-chicken-breast | Grilled chicken breast | חזה עוף על האש
+    'Grilled-chicken-breast': '',
+    // Greek-kebab | Greek kebab | קבב יווני
+    'Greek-kebab': '',
+    // Burger-on-Greek-pita | Burger on Greek pita | המבורגר על פיתה יוונית
+    'Burger-on-Greek-pita': '',
+
+    // Fish
+    // Sea-Bream-Dorade | Sea bream - Dorade | דניס / צ׳יפורה
+    'Sea-Bream-Dorade': 'images/menu/food/Sea-Bream-Dorade.png',
+    // Sea-Bass-Loup-De-Mer | Sea bass - Loup de mer | לברק
+    'Sea-Bass-Loup-De-Mer': 'images/menu/food/Sea-Bass-Loup-De-Mer.png',
+
+    // Vegan / Vegetarian
+    // Omelette-with-salad-and-pita | Omelette with salad & pita | חביתה + שתי פיתות + קערית סלט
+    'Omelette-with-salad-and-pita': '',
+    // Omelette-with-onion-or-mushrooms | Omelette with onion or mushrooms | חביתה עם בצל או פטריות + פיתות + סלט
+    'Omelette-with-onion-or-mushrooms': '',
+    // Greek-salad-with-vegan-cheese | Greek salad with vegan cheese | סלט יווני עם גבינה טבעונית + שתי פיתות
+    'Greek-salad-with-vegan-cheese': '',
+
+    // Soft Drinks
+    // Water-Soda | Water / Soda | מים / סודה
+    'Water-Soda': '',
+    // Coke-Zero | Cola / Zero | קולה / זירו
+    'Coke-Zero': 'images/menu/drinks/Coke-Zero.png',
+    // Sprite-Zero | Sprite / Zero | ספרייט / זירו
+    'Sprite-Zero': 'images/menu/drinks/Sprite-Zero.png',
+    // Fanta-Orange | Fanta Orange | פנטה תפוזים
+    'Fanta-Orange': 'images/menu/drinks/fanta.png',
+    // Ice-Tea-Peach | Ice Tea Peach | אייס תה אפרסק
+    'Ice-Tea-Peach': '',
+    // Juices-Orange-Lemonade-Cherry | Juices: Orange / Lemonade / Cherry | מיצים: תפוזים / לימונדה / דובדבן
+    'Juices-Orange-Lemonade-Cherry': '',
+    // Energy-drink | Energy drink | משקה אנרגיה
+    'Energy-drink': '',
+
+    // Alcoholic Drinks
+    // Heineken | Heineken | הייניקן
+    'Heineken': 'images/menu/alcohol/Heineken.png',
+    // Carlsberg | Carlsberg | קרלסברג
+    'Carlsberg': 'images/menu/alcohol/Carlsberg.png',
+    // Corona | Corona | קורונה
+    'Corona': 'images/menu/alcohol/Corona.png',
+    // Yasou-Ouzo-Cocktail | Yasou Ouzo Cocktail | יאסו קוקטייל אוזו
+    'Yasou-Ouzo-Cocktail': '',
+    // Non-alcoholic-fruit-mix | Non-alcoholic fruit mix | מיקס פירות ללא אלכוהול
+    'Non-alcoholic-fruit-mix': '',
+    // Plomari | Plomari | פלומרי
+    'Plomari': '',
+    // Ouzo-12 | Ouzo 12 | אוזו 12
+    'Ouzo-12': 'images/menu/alcohol/Ouzo12.png',
+    // Ouzo-200ml | Ouzo 200ml | אוזו 200 מ״ל
+    'Ouzo-200ml': 'images/menu/alcohol/Ouzo12.png',
+    // Jameson | Jameson | ג׳יימסון
+    'Jameson': 'images/menu/alcohol/Jameson.png',
+    // Jack-Daniel-s | Jack Daniel's | ג׳ק דניאל
+    'Jack-Daniel-s': '',
+    // Chivas | Chivas | שיבאס
+    'Chivas': '',
+    // Black-Label | Black Label | בלאק לייבל
+    'Black-Label': '',
+    // Stoli | Stoli | סטולי
+    'Stoli': '',
+    // Finlandia | Finlandia | פילנדיה
+    'Finlandia': 'images/menu/alcoholic/Finlandia.png',
+    // Grey-Goose | Grey Goose | גריי גוס
+    'Grey-Goose': '',
+
+    // Nargilla
+    // Nargilla-Apple-Watermelon-Mint | Nargilla: Apple / Watermelon / Mint | נרגילה: תפוח / אבטיח / נענע
+    'Nargilla-Apple-Watermelon-Mint': '',
+    // Refill-head-new-coal | Refill head + new coal | החלפת ראש + גחל חדש
+    'Refill-head-new-coal': '',
+
+    // Dessert
+    // Dessert-of-the-day | Dessert of the day | שאל את המלצר על קינוח היום!
+    'Dessert-of-the-day': ''
+};
