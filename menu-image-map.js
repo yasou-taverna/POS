c232477c69bdc73d70f5b8a9d9e023f126eacf97
Yasou Taverna <yasoutavern@gmail.com>
2026-08-21 00:33:51 -0400

    Fix image paths and update menu item categories
    Updated image paths for menu items and corrected category names.

--- menu-image-map.js
+++ menu-image-map.js
@@ -1,11 +1,11 @@
 // Menu image map
 // Put image files in:
 // - images/menu/food
-// - images/menu/drink
-// - images/menu/alcoholic
+// - images/menu/drinks
+// - images/menu/alcohol
 //
 // Example:
-// 'Coke-Zero': 'images/menu/drink/Coke-Zero.png',
+// 'Coke-Zero': 'images/menu/drinks/Coke-Zero.png',
 //
 // Keep empty strings until the real images arrive.
 
@@ -18,57 +18,57 @@ window.menuImageMap = {
     // Crispy-French-fries | Crispy French fries | צ׳יפס קריספי
     'Crispy-French-fries': 'images/menu/food/crispy-fries.png',
     // Fresh-vegetable-plate | Fresh vegetable plate | פלטת ירקות טריים
-    'Fresh-vegetable-plate': '',
+    'Fresh-vegetable-plate': 'images/menu/food/Fresh-vegetable-plate.png',
     // Rice-plate | Rice plate | צלחת אורז
-    'Rice-plate': '',
+    'Rice-plate': 'images/menu/food/Rice-plate.png',
 
     // Salads
     // Tahini-with-2-pita-breads | Tahini with 2 pita breads | טחינה בתוספת שתי פיתות
-    'Tahini-with-2-pita-breads': '',
+    'Tahini-with-2-pita-breads': 'images/menu/food/Tahini-with-2-pita-breads.png',
     // Eggplant | Eggplant | חצילים
-    'Eggplant': '',
+    'Eggplant': 'images/menu/food/Eggplant.png',
     // Eggplant-with-tahini | Eggplant with tahini | חצילים עם טחינה בצד
-    'Eggplant-with-tahini': '',
+    'Eggplant-with-tahini': 'images/menu/food/Eggplant-with-tahini.png',
     // Grilled-hot-peppers | Grilled hot peppers | פלפלים חריפים על האש
-    'Grilled-hot-peppers': '',
+    'Grilled-hot-peppers': 'images/menu/food/Grilled-hot-peppers.png',
     // Greek-olives | Greek olives | זיתים יווניים
-    'Greek-olives': '',
+    'Greek-olives': 'images/menu/food/Greek-olives.png',
     // Chopped-vegetable-salad | Chopped vegetable salad | סלט ירקות קצוץ
-    'Chopped-vegetable-salad': '',
+    'Chopped-vegetable-salad': 'images/menu/food/Chopped-vegetable-salad.png',
     // Yasou-salad | Yasou salad | סלט יאסו
-    'Yasou-salad': '',
+    'Yasou-salad': 'images/menu/food/Yasou-salad.png',
     // Vegan-Greek-Salad | Vegan Greek Salad | סלט יווני טבעוני
-    'Vegan-Greek-Salad': '',
+    'Vegan-Greek-Salad': 'images/menu/food/Vegan-Greek-Salad.png',
     // Extra-two-pita-breads | Extra two pita breads | תוספת 2 פיתות
-    'Extra-two-pita-breads': '',
+    'Extra-two-pita-breads': 'images/menu/food/Extra-two-pita-breads.png',
     // Extra-tahini-Zhug-hot-sauce | Extra tahini / Zhug hot sauce | תוספת טחינה / חריף סחוג
-    'Extra-tahini-Zhug-hot-sauce': '',
+    'Extra-tahini-Zhug-hot-sauce': 'images/menu/food/Extra-tahini-Zhug-hot-sauce.png',
 
     // Hummus
     // Home-made-hummus | Home-made hummus | חומוס ביתי
-    'Home-made-hummus': '',
+    'Home-made-hummus': 'images/menu/food/hummus.png',
     // Hummus-Tahini | Hummus Tahini | חומוס טחינה
-    'Hummus-Tahini': '',
+    'Hummus-Tahini': 'images/menu/food/hummus (2).png',
     // Hummus-with-mushrooms | Hummus with mushrooms | חומוס פטריות
-    'Hummus-with-mushrooms': '',
+    'Hummus-with-mushrooms': 'images/menu/food/Hummus-with-mushrooms.png',
     // Hummus-with-mini-schnitzel | Hummus with mini schnitzel | חומוס שניצלונים
-    'Hummus-with-mini-schnitzel': '',
+    'Hummus-with-mini-schnitzel': 'images/menu/food/Hummus-with-mini-schnitzel.png',
     // Hummus-with-minced-meat | Hummus with minced meat | חומוס בשר טחון
-    'Hummus-with-minced-meat': '',
+    'Hummus-with-minced-meat': 'images/menu/food/Hummus-with-minced-meat.png',
 
     // Meat & Chicken
     // Pargit-chicken-thighs | Pargit - chicken thighs | פרגיות
-    'Pargit-chicken-thighs': '',
+    'Pargit-chicken-thighs': 'images/menu/food/Pargit-chicken-thighs.png',
     // Schnitzel | Schnitzel | שניצל
-    'Schnitzel': '',
+    'Schnitzel': 'images/menu/food/Schnitzel.png',
     // Mini-schnitzel | Mini schnitzel | שניצלונים
-    'Mini-schnitzel': '',
+    'Mini-schnitzel': 'images/menu/food/Mini-schnitzel.png',
     // Grilled-chicken-breast | Grilled chicken breast | חזה עוף על האש
-    'Grilled-chicken-breast': '',
+    'Grilled-chicken-breast': 'images/menu/food/Grilled-chicken-breast.png',
     // Greek-kebab | Greek kebab | קבב יווני
-    'Greek-kebab': '',
+    'Greek-kebab': 'images/menu/food/Greek-kebab.png',
     // Burger-on-Greek-pita | Burger on Greek pita | המבורגר על פיתה יוונית
-    'Burger-on-Greek-pita': '',
+    'Burger-on-Greek-pita': 'images/menu/food/Burger-on-Greek-pita.png',
 
     // Fish
     // Sea-Bream-Dorade | Sea bream - Dorade | דניס / צ׳יפורה
@@ -78,15 +78,15 @@ window.menuImageMap = {
 
     // Vegan / Vegetarian
     // Omelette-with-salad-and-pita | Omelette with salad & pita | חביתה + שתי פיתות + קערית סלט
-    'Omelette-with-salad-and-pita': '',
+    'Omelette-with-salad-and-pita': 'images/menu/food/Omelette-with-salad-and-pita.png',
     // Omelette-with-onion-or-mushrooms | Omelette with onion or mushrooms | חביתה עם בצל או פטריות + פיתות + סלט
-    'Omelette-with-onion-or-mushrooms': '',
+    'Omelette-with-onion-or-mushrooms': 'images/menu/food/Omelette-with-onion-or-mushrooms.png',
     // Greek-salad-with-vegan-cheese | Greek salad with vegan cheese | סלט יווני עם גבינה טבעונית + שתי פיתות
-    'Greek-salad-with-vegan-cheese': '',
+    'Greek-salad-with-vegan-cheese': 'images/menu/food/Greek-salad-with-vegan-cheese.png',
 
     // Soft Drinks
     // Water-Soda | Water / Soda | מים / סודה
-    'Water-Soda': '',
+    'Water-Soda': 'images/menu/drinks/Water-Soda.png',
     // Coke-Zero | Cola / Zero | קולה / זירו
     'Coke-Zero': 'images/menu/drinks/Coke-Zero.png',
     // Sprite-Zero | Sprite / Zero | ספרייט / זירו
@@ -94,11 +94,11 @@ window.menuImageMap = {
     // Fanta-Orange | Fanta Orange | פנטה תפוזים
     'Fanta-Orange': 'images/menu/drinks/fanta.png',
     // Ice-Tea-Peach | Ice Tea Peach | אייס תה אפרסק
-    'Ice-Tea-Peach': '',
+    'Ice-Tea-Peach': 'images/menu/drinks/Ice-Tea-Peach.png',
     // Juices-Orange-Lemonade-Cherry | Juices: Orange / Lemonade / Cherry | מיצים: תפוזים / לימונדה / דובדבן
-    'Juices-Orange-Lemonade-Cherry': '',
+    'Juices-Orange-Lemonade-Cherry': 'images/menu/drinks/Juices-Orange-Lemonade-Cherry.png',
     // Energy-drink | Energy drink | משקה אנרגיה
-    'Energy-drink': '',
+    'Energy-drink': 'images/menu/drinks/Energy-drink.png',
 
     // Alcoholic Drinks
     // Heineken | Heineken | הייניקן
@@ -108,37 +108,38 @@ window.menuImageMap = {
     // Corona | Corona | קורונה
     'Corona': 'images/menu/alcohol/Corona.png',
     // Yasou-Ouzo-Cocktail | Yasou Ouzo Cocktail | יאסו קוקטייל אוזו
-    'Yasou-Ouzo-Cocktail': '',
+    'Yasou-Ouzo-Cocktail': 'images/menu/alcohol/Yasou-Ouzo-Cocktail.png',
     // Non-alcoholic-fruit-mix | Non-alcoholic fruit mix | מיקס פירות ללא אלכוהול
-    'Non-alcoholic-fruit-mix': '',
+    'Non-alcoholic-fruit-mix': 'images/menu/alcohol/Non-alcoholic-fruit-mix.png',
     // Plomari | Plomari | פלומרי
-    'Plomari': '',
+    'Plomari': 'images/menu/alcohol/Plomari.png',
     // Ouzo-12 | Ouzo 12 | אוזו 12
-    'Ouzo-12': 'images/menu/alcohol/Ouzo12.png',
+    'Ouzo-12': 'images/menu/alcohol/ouzo12.png',
     // Ouzo-200ml | Ouzo 200ml | אוזו 200 מ״ל
-    'Ouzo-200ml': 'images/menu/alcohol/Ouzo12.png',
+    'Ouzo-200ml': 'images/menu/alcohol/Ouzo-200ml.png',
     // Jameson | Jameson | ג׳יימסון
     'Jameson': 'images/menu/alcohol/Jameson.png',
     // Jack-Daniel-s | Jack Daniel's | ג׳ק דניאל
-    'Jack-Daniel-s': '',
+    'Jack-Daniel-s': 'images/menu/alcohol/Jack-Daniel-s.png',
     // Chivas | Chivas | שיבאס
-    'Chivas': '',
+    'Chivas': 'images/menu/alcohol/Chivas.png',
     // Black-Label | Black Label | בלאק לייבל
-    'Black-Label': '',
+    'Black-Label': 'images/menu/alcohol/Black-Label.png',
     // Stoli | Stoli | סטולי
-    'Stoli': '',
+    'Stoli': 'images/menu/alcohol/Stoli.png',
     // Finlandia | Finlandia | פילנדיה
-    'Finlandia': 'images/menu/alcoholic/Finlandia.png',
+    'Finlandia': 'images/menu/alcohol/Finlandia.png',
     // Grey-Goose | Grey Goose | גריי גוס
-    'Grey-Goose': '',
+    'Grey-Goose': 'images/menu/alcohol/Grey-Goose.png',
 
     // Nargilla
     // Nargilla-Apple-Watermelon-Mint | Nargilla: Apple / Watermelon / Mint | נרגילה: תפוח / אבטיח / נענע
-    'Nargilla-Apple-Watermelon-Mint': '',
+    'Nargilla-Apple-Watermelon-Mint': 'images/menu/food/Nargilla-Apple-Watermelon-Mint.png',
     // Refill-head-new-coal | Refill head + new coal | החלפת ראש + גחל חדש
-    'Refill-head-new-coal': '',
+    'Refill-head-new-coal': 'images/menu/food/Refill-head-new-coal.png',
 
     // Dessert
     // Dessert-of-the-day | Dessert of the day | שאל את המלצר על קינוח היום!
     'Dessert-of-the-day': ''
 };
+
